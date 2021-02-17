b9636a46e7e6290fd394122bcbbd1e419fe69ab1
ying-jeanne <wangyingensea@hotmail.com>
2021-02-17 12:59:16 +0100

    Revert "Make Datetime local (No date if today) working (#31274) (#31275)"
    This reverts commit 29e75ad97b61f09f4880e3d3adbda810bfbf5dbf.

--- packages/grafana-data/src/field/displayProcessor.ts
+++ packages/grafana-data/src/field/displayProcessor.ts
@@ -31,7 +31,6 @@ const timeFormats: KeyValue<boolean> = {
   dateTimeAsUS: true,
   dateTimeAsUSNoDateIfToday: true,
   dateTimeAsLocal: true,
-  dateTimeAsLocalNoDateIfToday: true,
   dateTimeFromNow: true,
 };
 
