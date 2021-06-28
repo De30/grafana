7a15324b8ec2e81c5a77db0ac6f77709f23fdb69
Christopher Lord <christopher@pliosoft.com>
2021-06-28 11:57:58 -0600

    more demo in loaded doc

--- public/app/features/storyboards/views/StoryboardListView.tsx
+++ public/app/features/storyboards/views/StoryboardListView.tsx
@@ -241,7 +241,26 @@ export const StoryboardListView = () => {
     sendMessage({
       type: 'NOTEBOOK_SET_INIT_DATA',
       payload: {
-        content: "# %% [javascript]\nconsole.log('Hello world!')",
+        content: `# %% [markdown]
+### More info
+We can do javascript:
+# %% [javascript]
+console.log('Hello world!')
+# %% [markdown]
+We can do python:
+# %% [python]
+print("hi, from python")
+# %% [markdown]
+Math should be no issue:
+# %% [latex]
+\\begin{equation}
+\\begin{aligned}
+\\frac{\\partial\\mathcal{D}}{\\partial t} \\quad & = \\quad \\nabla\\times\\mathcal{H},   & \\quad \\text{(Loi de Faraday)} \\\\[5pt]
+\\frac{\\partial\\mathcal{B}}{\\partial t} \\quad & = \\quad -\\nabla\\times\\mathcal{E},  & \\quad \\text{(Loi d'Ampère)}   \\\\[5pt]
+\\nabla\\cdot\\mathcal{B}                 \\quad & = \\quad 0,                         & \\quad \\text{(Loi de Gauss)}  \\\\[5pt]
+\\nabla\\cdot\\mathcal{D}                 \\quad & = \\quad 0.                         & \\quad \\text{(Loi de Colomb)}
+\\end{aligned}
+\\end{equation}`,
       },
     });
   }, [loaded, sendMessage]);
