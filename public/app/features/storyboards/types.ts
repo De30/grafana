df4e181caf457d9b05287c05929dd34165fb2307
Tobias Skarhed <tobias.skarhed@gmail.com>
2021-06-29 11:22:04 +0200

    Starboard: Refactor

--- public/app/features/storyboards/types.ts
+++ public/app/features/storyboards/types.ts
@@ -1,3 +1,10 @@
+import type {
+  ContentUpdateMessage,
+  ReadySignalMessage,
+  SaveMessage,
+  OutboundNotebookMessage,
+} from 'starboard-notebook';
+
 export enum CellTypes {
   Markdown = 'markdown',
   Snapshot = 'snapshot',
@@ -15,3 +22,39 @@ export interface Storyboard {
   id: string;
   cells: any[]; //Fix later
 }
+
+export type StarboardNotebookIFrameOptions<ReceivedMessageType = OutboundNotebookMessage> = {
+  src: string;
+  autoResize: boolean;
+  baseUrl?: string;
+  notebookContent?: Promise<string> | string;
+  onNotebookReadySignalMessage(payload: ReadySignalMessage['payload']): void;
+  onSaveMessage(payload: SaveMessage['payload']): void | boolean | Promise<boolean>;
+  onContentUpdateMessage(payload: ContentUpdateMessage['payload']): void;
+  onMessage(message: ReceivedMessageType): void;
+  onUnsavedChangesStatusChange(hasUnsavedChanges: boolean): void;
+  sandbox: string;
+  preventNavigationWithUnsavedChanges: boolean;
+};
+
+interface StarboardNotebookMessageSignalReady {
+  type: 'SIGNAL_READY';
+}
+
+interface StarboardNotebookMessageSetNotebookContent {
+  type: 'SET_NOTEBOOK_CONTENT';
+}
+
+interface StarboardNotebookMessageNotebookContentUpdate {
+  type: 'NOTEBOOK_CONTENT_UPDATE';
+}
+
+interface StarboardNotebookMessageSave {
+  type: 'SAVE';
+}
+
+export type StarboardNotebookMessage =
+  | StarboardNotebookMessageSignalReady
+  | StarboardNotebookMessageSetNotebookContent
+  | StarboardNotebookMessageNotebookContentUpdate
+  | StarboardNotebookMessageSave;
