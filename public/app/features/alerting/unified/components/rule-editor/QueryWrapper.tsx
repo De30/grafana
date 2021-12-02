f59b49db75d98e7129a029915d3558a25bfa7b16
Peter Holmberg <peter.hlmbrg@gmail.com>
2021-12-02 17:20:15 +0100

    progress on form elements

--- public/app/features/alerting/unified/components/rule-editor/QueryWrapper.tsx
+++ public/app/features/alerting/unified/components/rule-editor/QueryWrapper.tsx
@@ -12,7 +12,7 @@ import {
   RelativeTimeRange,
   ThresholdsConfig,
 } from '@grafana/data';
-import { RelativeTimeRangePicker, useStyles2 } from '@grafana/ui';
+import { Icon, InlineField, Input, RelativeTimeRangePicker, useStyles2 } from '@grafana/ui';
 import { QueryEditorRow } from 'app/features/query/components/QueryEditorRow';
 import { VizWrapper } from './VizWrapper';
 import { isExpressionQuery } from 'app/features/expressions/guards';
@@ -54,8 +54,9 @@ export const QueryWrapper: FC<Props> = ({
   const styles = useStyles2(getStyles);
   const isExpression = isExpressionQuery(query.model);
   const [pluginId, changePluginId] = useState<SupportedPanelPlugins>(isExpression ? TABLE : TIMESERIES);
+  const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
 
-  const renderTimePicker = (query: AlertQuery, index: number): ReactNode => {
+  const renderTimePicker = (): ReactNode => {
     if (isExpressionQuery(query.model) || !onChangeTimeRange) {
       return null;
     }
@@ -65,6 +66,56 @@ export const QueryWrapper: FC<Props> = ({
         timeRange={query.relativeTimeRange ?? getDefaultRelativeTimeRange()}
         onChange={(range) => onChangeTimeRange(range, index)}
       />
+    );
+  };
+
+  const renderQueryOptions = () => {
+    return (
+      <div className={styles.queryOptions}>
+        <div onClick={() => setOptionsOpen(!optionsOpen)}>
+          Query options <Icon name={optionsOpen ? 'angle-left' : 'angle-right'} />
+        </div>
+        {optionsOpen ? (
+          <>
+            <InlineField
+              label="Min interval"
+              tooltip={
+                <>
+                  A lower limit for the interval. Recommended to be set to write frequency, for example <code>1m</code>{' '}
+                  if your data is written every minute. Default value can be set in data source settings for most data
+                  sources.
+                </>
+              }
+            >
+              <Input placeholder="15s" />
+            </InlineField>
+            <InlineField
+              label="Max data points"
+              tooltip={
+                <>
+                  The maximum data points per series. Used directly by some data sources and used in calculation of auto
+                  interval. With streaming data this value is used for the rolling buffer.
+                </>
+              }
+            >
+              <Input />
+            </InlineField>
+          </>
+        ) : null}
+      </div>
+    );
+  };
+
+  const renderHeaderExtras = () => {
+    if (isExpressionQuery(query.model)) {
+      return null;
+    }
+
+    return (
+      <div className={styles.headerExtras}>
+        {renderTimePicker()}
+        {renderQueryOptions()}
+      </div>
     );
   };
 
@@ -83,7 +134,7 @@ export const QueryWrapper: FC<Props> = ({
         onAddQuery={() => onDuplicateQuery(cloneDeep(query))}
         onRunQuery={onRunQueries}
         queries={queries}
-        renderHeaderExtras={() => renderTimePicker(query, index)}
+        renderHeaderExtras={renderHeaderExtras}
         app={CoreApp.UnifiedAlerting}
         visualization={
           data.state !== LoadingState.NotStarted ? (
@@ -109,5 +160,14 @@ const getStyles = (theme: GrafanaTheme2) => ({
     border: 1px solid ${theme.colors.border.medium};
     border-radius: ${theme.shape.borderRadius(1)};
     padding-bottom: ${theme.spacing(1)};
+  `,
+  queryOptions: css`
+    display: flex;
+    margin-left: ${theme.spacing(1)};
+  `,
+  headerExtras: css`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
   `,
 });
