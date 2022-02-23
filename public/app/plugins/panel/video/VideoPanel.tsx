211681ae66486c0067513cffce56f733716977c7
Ryan McKinley <ryantxu@gmail.com>
2022-02-22 18:25:01 -0800

    with sync events

--- public/app/plugins/panel/video/VideoPanel.tsx
+++ public/app/plugins/panel/video/VideoPanel.tsx
@@ -1,47 +1,86 @@
-import React from 'react';
-import { PanelProps } from '@grafana/data';
+import React, { PureComponent } from 'react';
+import { EventBus, DataHoverEvent, PanelProps } from '@grafana/data';
+import { Subscription } from 'rxjs';
 import { PanelOptions } from './models.gen';
 import videojs from 'video.js';
-import VideoJS from './VideoJS';
+import VideoJSWrapper from './VideoJSWrapper';
+import { VideoPlayPayload } from './types';
 
-interface VideoPanelProps extends PanelProps<PanelOptions> {}
+interface Props extends PanelProps<PanelOptions> {}
 
-export const HeatmapPanel: React.FC<VideoPanelProps> = ({
-  data,
-  id,
-  timeRange,
-  timeZone,
-  width,
-  height,
-  options,
-  fieldConfig,
-  onChangeTimeRange,
-  replaceVariables,
-}) => {
-  const playerRef = React.useRef<videojs.Player>();
+interface State {
+  over?: boolean;
+  opts: videojs.PlayerOptions;
+  duration: number; // seconds;
+}
 
-  const videoJsOptions = {
-    // lookup the options in the docs for more options
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    mute: true,
-    sources: [
-      {
-        //        src: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
-        // src: 'https://storage.googleapis.com/grafana-downloads/files/temp/PXL-20220122.mp4',
-        src: 'https://storage.googleapis.com/grafana-downloads/files/temp/sample-10s.mp4',
-        //src: 'http://localhost:3000/public/videos/skate.mp4',
-        type: 'video/mp4',
+export class VideoPanel extends PureComponent<Props, State> {
+  player?: videojs.Player;
+  subscription = new Subscription();
+  eventBus: EventBus;
+  hoverPayload: VideoPlayPayload = { point: {} as any };
+  hoverEvent = new DataHoverEvent(this.hoverPayload);
+
+  constructor(props: Props) {
+    super(props);
+
+    this.eventBus = props.eventBus.newScopedBus('local', { onlyLocal: true });
+    this.state = {
+      over: false,
+      duration: 0,
+      opts: {
+        // lookup the options in the docs for more options
+        autoplay: true,
+        controls: true,
+        responsive: true,
+        fluid: true,
+        fill: true,
+        muted: true,
+        sources: [
+          {
+            //        src: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+            // src: 'https://storage.googleapis.com/grafana-downloads/files/temp/PXL-20220122.mp4',
+            src: 'https://storage.googleapis.com/grafana-downloads/files/temp/sample-10s.mp4',
+            //src: 'http://localhost:3000/public/videos/skate.mp4',
+            type: 'video/mp4',
+          },
+        ],
+        width: props.width,
+        height: props.height,
       },
-    ],
-    width,
-    height,
-  };
+    };
+  }
 
-  const handlePlayerReady = (player: videojs.Player) => {
-    playerRef.current = player;
+  componentDidMount() {
+    const { eventBus } = this.props;
+
+    this.subscription.add(
+      eventBus.subscribe(DataHoverEvent, (event) => {
+        if (event.origin === this.eventBus) {
+          return;
+        }
+
+        const { duration } = this.state;
+        const time = event.payload?.point?.time;
+        if (time && duration && this.player) {
+          const start = this.props.data.timeRange.from.valueOf();
+          const offset = (time - start) / 1000; // ms > s
+          if (offset <= duration) {
+            console.log('OTHER HOVER', time);
+            this.player.pause(); // do not keep playing
+            this.player.currentTime(offset);
+          }
+        }
+      })
+    );
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+  handlePlayerReady = (player: videojs.Player) => {
+    this.player = player;
     console.log('ready', player);
 
     player.on('waiting', (v: any) => {
@@ -52,36 +91,55 @@ export const HeatmapPanel: React.FC<VideoPanelProps> = ({
       console.log('progress', v);
     });
 
+    player.on('seeking', (v: any) => {
+      console.log('seeking', v);
+    });
+
+    player.on('seeked', (v: any) => {
+      console.log('seeked', v);
+    });
+
+    player.on('ended', (v: any) => {
+      console.log('ended', v);
+    });
+
+    player.on('stageclick', (v: any) => {
+      console.log('stageclick', v);
+    });
+
     player.on('loadedmetadata', (v: any) => {
       console.log('loadedmetadata', v);
     });
 
     player.on('durationchange', (v: any) => {
       console.log('durationchange', v);
+      this.setState({ duration: player.duration() });
     });
 
     player.on('timeupdate', (v: any) => {
-      console.log('timeupdate', { durration: player.duration(), xxx: player.currentTime(), jj: player.toJSON() });
+      const evt = this.hoverPayload;
+      const start = this.props.data.timeRange.from.valueOf();
+      const offset = player.currentTime();
+
+      evt.point.time = offset * 1000 + start;
+      evt.point.offset = offset; // seconds
+      evt.point.duration = player.duration();
+
+      this.eventBus.publish(this.hoverEvent);
     });
 
     player.on('dispose', () => {
       console.log('player will dispose');
+      this.player = undefined;
     });
   };
 
-  // const changePlayerOptions = () => {
-  //   // you can update the player through the Video.js player instance
-  //   if (!playerRef.current) {
-  //     return;
-  //   }
-  //   // [update player through instance's api]
-  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
-  //   playerRef.current.autoplay(false);
-  // };
-
-  return (
-    <>
-      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
-    </>
-  );
-};
+  render() {
+    const { opts } = this.state;
+    return (
+      <div>
+        <VideoJSWrapper options={opts} onReady={this.handlePlayerReady} />
+      </div>
+    );
+  }
+}
--- public/app/plugins/panel/video/module.tsx
+++ public/app/plugins/panel/video/module.tsx
@@ -1,10 +1,10 @@
 import { GraphFieldConfig } from '@grafana/schema';
 import { PanelPlugin } from '@grafana/data';
-import { HeatmapPanel } from './VideoPanel';
 import { PanelOptions, defaultPanelOptions, defaultPanelFieldConfig } from './models.gen';
 import { getVideoFieldConfig } from './config';
+import { VideoPanel } from './VideoPanel';
 
-export const plugin = new PanelPlugin<PanelOptions, GraphFieldConfig>(HeatmapPanel)
+export const plugin = new PanelPlugin<PanelOptions, GraphFieldConfig>(VideoPanel)
   .useFieldConfig(getVideoFieldConfig(defaultPanelFieldConfig))
   .setPanelOptions((builder, context) => {
     const opts = context.options ?? defaultPanelOptions;
