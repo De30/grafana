a0b016d9406f8fd8a7193d1747784a52890e176c
gillesdemey <gilles.de.mey@gmail.com>
2022-03-04 15:39:08 +0100

    hackathon: wip summary

--- packages/grafana-ui/src/components/UptimeSummary/UptimeSummary.story.tsx
+++ packages/grafana-ui/src/components/UptimeSummary/UptimeSummary.story.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { UptimeSummary, UptimeSummaryProps } from './UptimeSummary';
+import { Incident, IncidentType, UptimeSummary, UptimeSummaryProps } from './UptimeSummary';
 
 export const AllOperational: Story<UptimeSummaryProps> = () => {
   return (
@@ -11,9 +11,46 @@ export const AllOperational: Story<UptimeSummaryProps> = () => {
 };
 
 export const WithIncident: Story<UptimeSummaryProps> = () => {
+  const incident: Incident = {
+    title: 'Some service is kinda slow',
+    description: 'We are looking in to it and will keep you updated.',
+    type: IncidentType.Degraded,
+    startTime: new Date(),
+  };
+
   return (
     <div>
-      <UptimeSummary incidents={[]} />
+      <UptimeSummary incidents={[incident]} />
+    </div>
+  );
+};
+
+export const WithOutage: Story<UptimeSummaryProps> = () => {
+  const outage: Incident = {
+    title: 'Severe outage',
+    description: "We don't really know what's going on either, it's total chaos over here.",
+    type: IncidentType.Outage,
+    startTime: new Date(),
+  };
+
+  return (
+    <div>
+      <UptimeSummary incidents={[outage]} />
+    </div>
+  );
+};
+
+export const WithMaintenance: Story<UptimeSummaryProps> = () => {
+  const outage: Incident = {
+    title: 'Just some maintenance',
+    description: "No real reason to panic. It's all part of the plan.",
+    type: IncidentType.Maintenance,
+    startTime: new Date(),
+  };
+
+  return (
+    <div>
+      <UptimeSummary incidents={[outage]} />
     </div>
   );
 };
--- packages/grafana-ui/src/components/UptimeSummary/UptimeSummary.tsx
+++ packages/grafana-ui/src/components/UptimeSummary/UptimeSummary.tsx
@@ -2,8 +2,9 @@ import React, { FC } from 'react';
 import { css, cx } from '@emotion/css';
 import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2 } from '../../themes';
+import { Icon } from '../Icon/Icon';
 
-interface Incident {
+export interface Incident {
   title: string;
   description?: string;
   updates?: IncidentUpdate[];
@@ -12,7 +13,7 @@ interface Incident {
   endTime?: Date;
 }
 
-enum IncidentType {
+export enum IncidentType {
   Maintenance = 'maintenance',
   Degraded = 'degraded',
   Outage = 'outage',
@@ -29,23 +30,136 @@ export interface UptimeSummaryProps {
 
 const UptimeSummary: FC<UptimeSummaryProps> = ({ incidents = [] }) => {
   const styles = useStyles2(getStyles);
+  const hasIncidents = incidents.length > 0;
+  const hasOutages = hasIncidents && incidents.some((incident) => incident.type === IncidentType.Outage);
+  const hasDegraded = hasIncidents && incidents.some((incident) => incident.type === IncidentType.Degraded);
+  const justMaintenance = hasIncidents && incidents.every((incident) => incident.type === IncidentType.Maintenance);
 
-  if (incidents.length === 0) {
-    return <div className={cx(styles.wrapper, styles.allOperational)}>All systems operational</div>;
+  // the most severe type in the incidents list chooses the entire summary type
+  let summaryType: IncidentType | undefined = undefined;
+
+  if (hasOutages) {
+    summaryType = IncidentType.Outage;
   }
 
-  return <div></div>;
+  if (hasDegraded) {
+    summaryType = IncidentType.Degraded;
+  }
+
+  if (justMaintenance) {
+    summaryType = IncidentType.Maintenance;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {<Header type={summaryType} />}
+      {hasIncidents && <SummaryItems incidents={incidents} type={summaryType} />}
+    </div>
+  );
+};
+
+interface HeaderProps {
+  type?: IncidentType;
+}
+
+const Header: FC<HeaderProps> = ({ type }) => {
+  const styles = useStyles2(getStyles);
+
+  if (type === IncidentType.Maintenance) {
+    return (
+      <div className={cx(styles.header.wrapper, styles.header.maintenance)}>
+        <Icon name="cog" /> Scheduled maintenance
+      </div>
+    );
+  }
+
+  if (type === IncidentType.Degraded) {
+    return (
+      <div className={cx(styles.header.wrapper, styles.header.degraded)}>
+        <Icon name="exclamation-triangle" /> Degraded service
+      </div>
+    );
+  }
+
+  if (type === IncidentType.Outage) {
+    return (
+      <div className={cx(styles.header.wrapper, styles.header.outage)}>
+        <Icon name="fire" /> Severe service impact
+      </div>
+    );
+  }
+
+  return (
+    <div className={cx(styles.header.wrapper, styles.header.allOperational)}>
+      <Icon name="check-circle" /> All systems operational
+    </div>
+  );
+};
+
+interface SummaryItemsProps {
+  type?: IncidentType;
+  incidents: Incident[];
+}
+
+const SummaryItems: FC<SummaryItemsProps> = ({ type = IncidentType.Degraded, incidents }) => {
+  const styles = useStyles2(getStyles);
+
+  return (
+    <div className={cx(styles.summaryItems.wrapper, styles.summaryItems[type])}>
+      {incidents.map((incident) => (
+        <div key={incident.startTime.toString()}>
+          <div>{incident.title}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const getStyles = (theme: GrafanaTheme2) => ({
   wrapper: css`
     display: flex;
-    padding: ${theme.spacing(1)} ${theme.spacing(2)};
+    flex-direction: column;
+    min-width: 500px;
   `,
-  allOperational: css`
-    border: solid 1px green;
-    background: green;
-  `,
+  header: {
+    wrapper: css`
+      padding: ${theme.spacing(1)} ${theme.spacing(2)};
+    `,
+    allOperational: css`
+      border: solid 1px ${theme.colors.success.border};
+      background: ${theme.colors.success.main};
+    `,
+    [IncidentType.Maintenance]: css`
+      border: solid 1px ${theme.colors.info.border};
+      background: ${theme.colors.info.main};
+    `,
+    [IncidentType.Degraded]: css`
+      border: solid 1px ${theme.colors.warning.border};
+      background: ${theme.colors.warning.main};
+      color: black;
+    `,
+    [IncidentType.Outage]: css`
+      border: solid 1px ${theme.colors.error.border};
+      background: ${theme.colors.error.main};
+    `,
+  },
+  summaryItems: {
+    wrapper: css`
+      padding: ${theme.spacing(1)} ${theme.spacing(2)};
+    `,
+    [IncidentType.Degraded]: css`
+      border: solid 1px ${theme.colors.warning.border};
+      border-top: none;
+    `,
+    [IncidentType.Outage]: css`
+      border: solid 1px ${theme.colors.error.border};
+      border-top: none;
+    `,
+    [IncidentType.Maintenance]: css`
+      border: solid 1px ${theme.colors.info.border};
+      border-top: none;
+    `,
+  },
 });
 
 export { UptimeSummary };
