d112a45a929851a9918836e6e127a61c4600d250
Jack Westbrook <jack.westbrook@gmail.com>
2022-05-06 12:00:36 +0200

    feat(grafana-data): use esbuild and plugin-dts for builds

--- packages/grafana-data/rollup.config.ts
+++ packages/grafana-data/rollup.config.ts
@@ -1,44 +1,80 @@
-import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import path from 'path';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+// import sourceMaps from 'rollup-plugin-sourcemaps';
+// import { terser } from 'rollup-plugin-terser';
 
-const pkg = require('./package.json');
+// const buildCjsPackage = ({ env }) => {
+//   return {
+//     input: `compiled/index.js`,
+//     output: [
+//       {
+//         file: `dist/index.${env}.js`,
+//         name: libraryName,
+//         format: 'cjs',
+//         sourcemap: true,
+//         exports: 'named',
+//         globals: {},
+//       },
+//     ],
+//     external: [
+//       'lodash',
+//       'rxjs',
+//       '@grafana/schema', // Load from host
+//     ],
+//     plugins: [
+//       resolve(),
+//       json({
+//         include: [path.relative('.', require.resolve('moment-timezone/data/packed/latest.json'))], // absolute path throws an error for whatever reason
+//       }),
+//       commonjs({
+//         include: /node_modules/,
+//       }),
+//       resolve(),
+//       sourceMaps(),
+//       env === 'production' && terser(),
+//     ],
+//   };
+// };
 
-const libraryName = pkg.name;
+const name = require('./package.json').main.replace(/\.js$/, '');
 
-const buildCjsPackage = ({ env }) => {
-  return {
-    input: `compiled/index.js`,
-    output: [
-      {
-        file: `dist/index.${env}.js`,
-        name: libraryName,
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'named',
-        globals: {},
-      },
-    ],
-    external: [
-      'lodash',
-      'rxjs',
-      '@grafana/schema', // Load from host
-    ],
+const bundle = (config) => ({
+  ...config,
+  input: 'src/index.ts',
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
     plugins: [
       resolve(),
       json({
-        include: [path.relative('.', require.resolve('moment-timezone/data/packed/latest.json'))], // absolute path throws an error for whatever reason
+        // absolute path throws an error for whatever reason
+        include: [path.relative('.', require.resolve('moment-timezone/data/packed/latest.json'))],
       }),
-      commonjs({
-        include: /node_modules/,
-      }),
-      resolve(),
-      sourceMaps(),
-      env === 'production' && terser(),
+      esbuild(),
     ],
-  };
-};
-export default [buildCjsPackage({ env: 'development' }), buildCjsPackage({ env: 'production' })];
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+];
