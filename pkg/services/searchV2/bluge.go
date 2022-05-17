c8d861ccdd1333e1c445ed48bf13afa6556e264d
Alexander Emelin <frvzmb@gmail.com>
2022-05-17 14:19:52 +0300

    search extension tweaks

--- pkg/services/searchV2/bluge.go
+++ pkg/services/searchV2/bluge.go
@@ -244,7 +244,7 @@ func getDashboardPanelIDs(reader *bluge.Reader, dashboardUID string) ([]string, 
 }
 
 //nolint: gocyclo
-func doSearchQuery(ctx context.Context, logger log.Logger, reader *bluge.Reader, filter ResourceFilter, q DashboardQuery, extender DashboardIndexExtender) *backend.DataResponse {
+func doSearchQuery(ctx context.Context, logger log.Logger, reader *bluge.Reader, filter ResourceFilter, q DashboardQuery, extender QueryExtender) *backend.DataResponse {
 	response := &backend.DataResponse{}
 
 	// Folder listing structure
@@ -435,10 +435,6 @@ func doSearchQuery(ctx context.Context, logger log.Logger, reader *bluge.Reader,
 			// 	vals[field] = string(value)
 			// }
 
-			if !ext(field, value) {
-				return false
-			}
-
 			switch field {
 			case documentFieldUID:
 				uid = string(value)
@@ -456,6 +452,10 @@ func doSearchQuery(ctx context.Context, logger log.Logger, reader *bluge.Reader,
 				ds_uids = append(ds_uids, string(value))
 			case documentFieldTag:
 				tags = append(tags, string(value))
+			default:
+				if !ext(field, value) {
+					return false
+				}
 			}
 			return true
 		})
