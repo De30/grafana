aa3a20b1d789eac3dd8e8950aeeb7b426a521d2a
Gilles De Mey <gilles.de.mey@gmail.com>
2022-06-13 22:18:52 +0200

    chore: hook up to data

--- public/app/features/alerting/unified/components/rules/RuleListListView.tsx
+++ public/app/features/alerting/unified/components/rules/RuleListListView.tsx
@@ -4,11 +4,16 @@ import React, { FC } from 'react';
 import { GrafanaTheme2 } from '@grafana/data';
 import { Stack } from '@grafana/experimental';
 import { Button, Icon, Pagination, Tag, useStyles2 } from '@grafana/ui';
-import { CombinedRuleNamespace } from 'app/types/unified-alerting';
+import { CombinedRule, CombinedRuleNamespace } from 'app/types/unified-alerting';
+import { GrafanaAlertState, PromAlertingRuleState, PromRuleType } from 'app/types/unified-alerting-dto';
 
 interface Props {
   namespaces: CombinedRuleNamespace[];
   expandAll: boolean;
+}
+
+function filterAlertStates(rules: CombinedRule[], state: PromAlertingRuleState) {
+  return rules.filter((rule) => rule.promRule?.type === PromRuleType.Alerting && rule.promRule?.state === state);
 }
 
 /**
@@ -20,6 +25,15 @@ interface Props {
 export const RuleListListView: FC<Props> = ({ namespaces }) => {
   const styles = useStyles2(getStyles);
 
+  const allRules = namespaces.flatMap((namespace) => namespace.groups.flatMap((group) => group.rules));
+  const alertRules = allRules.filter((rule) => rule.promRule?.type === PromRuleType.Alerting);
+
+  const GROUPED_BY_STATE = {
+    [GrafanaAlertState.Alerting]: filterAlertStates(alertRules, PromAlertingRuleState.Firing),
+    [GrafanaAlertState.Pending]: filterAlertStates(alertRules, PromAlertingRuleState.Pending),
+    [GrafanaAlertState.Normal]: filterAlertStates(alertRules, PromAlertingRuleState.Inactive),
+  };
+
   return (
     <>
       <div className={styles.list.wrapper}>
@@ -28,16 +42,16 @@ export const RuleListListView: FC<Props> = ({ namespaces }) => {
             <Stack alignItems={'center'} gap={0.5}>
               <Icon name="fire" size="sm" />
               <Shiny>
-                <strong>153 Firing</strong>
+                <strong>{GROUPED_BY_STATE[GrafanaAlertState.Alerting].length} Firing</strong>
               </Shiny>
             </Stack>
             <Stack alignItems={'center'} gap={0.5}>
               <Icon name="check-circle" size="sm" />
-              2,923 Normal
+              {GROUPED_BY_STATE[GrafanaAlertState.Normal].length} Normal
             </Stack>
             <Stack alignItems={'center'} gap={0.5}>
               <Icon name="circle" size="sm" />
-              47 Pending
+              {GROUPED_BY_STATE[GrafanaAlertState.Pending].length} Pending
             </Stack>
             <Spacer />
             <Stack alignItems={'center'} gap={0.25}>
@@ -57,103 +71,65 @@ export const RuleListListView: FC<Props> = ({ namespaces }) => {
         <div className={styles.list.body.wrapper}>
           <Stack direction="column" gap={0}>
             {/* start row item */}
-            <div className={styles.list.body.row}>
-              <Stack alignItems={'baseline'}>
-                <Icon name="fire" size="lg" />
-                <Stack direction="column" gap={0.25}>
-                  {/* Name and tags */}
-                  <Stack gap={0.5} alignItems={'center'}>
-                    <Shiny>
-                      <strong>CPU Usage</strong>
-                    </Shiny>
-                    <Stack alignItems={'center'} gap={0.5}>
-                      <Tag name="team=operations" /> <Tag name="type=CPU" />
+            {GROUPED_BY_STATE[GrafanaAlertState.Alerting].map((rule) => (
+              <div key={rule.namespace.name + rule.group.name + rule.name} className={styles.list.body.row}>
+                <Stack alignItems={'baseline'}>
+                  {rule.promRule?.type === PromRuleType.Alerting &&
+                    rule.promRule?.state === PromAlertingRuleState.Firing && <Icon name="fire" size="lg" />}
+                  {rule.promRule?.type === PromRuleType.Alerting &&
+                    rule.promRule?.state === PromAlertingRuleState.Inactive && <Icon name="check-circle" size="lg" />}
+                  {rule.promRule?.type === PromRuleType.Alerting &&
+                    rule.promRule?.state === PromAlertingRuleState.Pending && <Icon name="circle" size="lg" />}
+                  <Stack direction="column" gap={0.25}>
+                    {/* Name and tags */}
+                    <Stack gap={0.5} alignItems={'center'}>
+                      <Shiny>
+                        <strong>{rule.name}</strong>
+                      </Shiny>
+                      <Stack alignItems={'center'} gap={0.5}>
+                        {Object.entries(rule.labels).map(([name, value]) => (
+                          <Tag key={name} name={`${name}=${value}`} />
+                        ))}
+                      </Stack>
+                    </Stack>
+                    <Stack alignItems={'center'} gap={1}>
+                      <Stack gap={0.5} alignItems={'center'}>
+                        <Icon name="folder" size="sm" />{' '}
+                        <small>
+                          {rule.namespace.name} / {rule.group.name}
+                        </small>
+                      </Stack>
+                      <Stack gap={0.5} alignItems={'center'}>
+                        <Icon name="database" size="sm" />
+                        <small>gdev-prometheus</small>
+                      </Stack>
+                      <Stack gap={0.5} alignItems={'center'}>
+                        <Icon name="clock-nine" size="sm" /> <small>for 4 minutes</small>
+                      </Stack>
                     </Stack>
                   </Stack>
-                  <Stack alignItems={'center'} gap={1}>
-                    <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="folder" size="sm" /> <small>Server Monitoring / My Group</small>
+                  <Spacer />
+                  <Stack alignItems={'center'}>
+                    <Stack gap={0.25} alignItems={'center'}>
+                      <Muted>
+                        <Icon name="layer-group" /> <small>{5}</small>
+                      </Muted>
                     </Stack>
                     <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="database" size="sm" />
-                      <small>gdev-prometheus</small>
-                    </Stack>
-                    <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="clock-nine" size="sm" /> <small>for 4 minutes</small>
+                      <Button icon="book" size="sm">
+                        View runbook
+                      </Button>
+                      <Button icon="apps" size="sm">
+                        View panel
+                      </Button>
+                      <Button icon="eye" size="sm">
+                        Details
+                      </Button>
                     </Stack>
                   </Stack>
                 </Stack>
-                <Spacer />
-                <Stack alignItems={'center'}>
-                  <Stack gap={0.25} alignItems={'center'}>
-                    <Muted>
-                      <Icon name="layer-group" /> <small>{5}</small>
-                    </Muted>
-                  </Stack>
-                  <Stack gap={0.5} alignItems={'center'}>
-                    <Button icon="book" size="sm">
-                      View runbook
-                    </Button>
-                    <Button icon="apps" size="sm">
-                      View panel
-                    </Button>
-                    <Button icon="eye" size="sm">
-                      Details
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Stack>
-            </div>
-            {/* end row item */}
-
-            {/* start row item */}
-            <div className={styles.list.body.row}>
-              <Stack alignItems={'baseline'}>
-                <Icon name="fire" size="lg" />
-                <Stack direction="column" gap={0.25}>
-                  {/* Name and tags */}
-                  <Stack gap={0.5} alignItems={'center'}>
-                    <Shiny>
-                      <strong>CPU Usage</strong>
-                    </Shiny>
-                    <Stack alignItems={'center'} gap={0.5}>
-                      <Tag name="team=operations" /> <Tag name="type=CPU" />
-                    </Stack>
-                  </Stack>
-                  <Stack alignItems={'center'} gap={1}>
-                    <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="folder" size="sm" /> <small>Server Monitoring / My Group</small>
-                    </Stack>
-                    <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="database" size="sm" />
-                      <small>gdev-prometheus</small>
-                    </Stack>
-                    <Stack gap={0.5} alignItems={'center'}>
-                      <Icon name="clock-nine" size="sm" /> <small>for 4 minutes</small>
-                    </Stack>
-                  </Stack>
-                </Stack>
-                <Spacer />
-                <Stack alignItems={'center'}>
-                  <Stack gap={0.25} alignItems={'center'}>
-                    <Muted>
-                      <Icon name="layer-group" /> <small>{5}</small>
-                    </Muted>
-                  </Stack>
-                  <Stack gap={0.5} alignItems={'center'}>
-                    <Button icon="book" size="sm">
-                      View runbook
-                    </Button>
-                    <Button icon="apps" size="sm">
-                      View panel
-                    </Button>
-                    <Button icon="eye" size="sm">
-                      Details
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Stack>
-            </div>
+              </div>
+            ))}
             {/* end row item */}
           </Stack>
         </div>
