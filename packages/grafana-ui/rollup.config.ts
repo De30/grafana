2156e1efc84b8ae2d8a07eb6de75e5be43e910e1
Jack Westbrook <jack.westbrook@gmail.com>
2022-06-28 14:51:22 +0200

    feat(grafana-ui): preserve modules for esm tree shaking

--- packages/grafana-ui/rollup.config.ts
+++ packages/grafana-ui/rollup.config.ts
@@ -1,41 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve';
+import path from 'path';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
+import { externals } from 'rollup-plugin-node-externals';
 import svg from 'rollup-plugin-svg-import';
 
-const name = require('./package.json').main.replace(/\.js$/, '');
+const pkg = require('./package.json');
+const name = pkg.main.replace(/\.js$/, '');
 
 const bundle = (config) => ({
   input: 'src/index.ts',
-  external: (id) => !/^[./]/.test(id),
+  plugins: [externals({ deps: true, packagePath: './package.json' }), resolve(), svg({ stringify: true }), esbuild()],
   ...config,
 });
 
 export default [
   bundle({
-    plugins: [resolve(), svg({ stringify: true }), esbuild()],
     output: [
       {
-        dir: './dist',
         format: 'cjs',
         sourcemap: true,
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        globals: {
-          react: 'React',
-          'prop-types': 'PropTypes',
-        },
+        dir: path.dirname(pkg.main),
       },
       {
-        dir: './dist/esm',
-        format: 'es',
+        format: 'esm',
         sourcemap: true,
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        globals: {
-          react: 'React',
-          'prop-types': 'PropTypes',
-        },
+        dir: path.dirname(pkg.module),
+        preserveModules: true,
+        // @ts-expect-error
+        preserveModulesRoot: path.join(process.env.PROJECT_CWD, `packages/grafana-ui/src`),
       },
     ],
   }),
