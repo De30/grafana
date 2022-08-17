061fc0260cc25a532f15457cfcf609a2916a5363
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-08-17 00:02:22 -0300

    Add EventHandler module
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- pkg/services/outgoingevents/models.go
+++ pkg/services/outgoingevents/models.go
@@ -1,5 +1,7 @@
 package outgoingevents
 
+import "time"
+
 type Event string
 
 const (
@@ -7,3 +9,31 @@ const (
 	EventIncidentResolved              = "incident.resolved"
 	EventIncidentSeverityChanged       = "incident.severityChanged"
 )
+
+// EventHandler refers to a script or webhook that should be used for
+// handling an event.
+type EventHandler struct {
+	ID     int64  `xorm:"pk autoincr 'id'" json:"id"`
+	UserID int64  `xorm:"user_id"`
+	Name   string `xorm:"name"`
+
+	// Event is the event name to handle.
+	Event Event `xorm:"event" json:"event"`
+
+	// Source is the source code of the handler to run in our scripts
+	// runner.
+	Source []byte `xorm:"source" json:"source,omitempty"`
+
+	// Language is the language of the script.
+	Language string `orm:"lang" json:"lang,omitempty"`
+
+	// WebhookURL is the URL of an external webhook.
+	WebhookURL string `xorm:"webhook_url" json:"webhook_url,omitempty"`
+
+	Created time.Time
+	Updated time.Time
+}
+
+func (h EventHandler) IsScript() bool  { return len(h.Source) > 0 && h.Language != "" }
+func (h EventHandler) IsWebhook() bool { return h.WebhookURL != "" }
+func (h EventHandler) IsValid() bool   { return h.IsScript() != h.IsWebhook() }
