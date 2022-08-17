55022479e371a6d95cbbb7b7d0e06083149537d1
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-08-17 00:09:06 -0300

    Add OutgoingEventsService.Publish skeleton
    Currently is missing sending the event payload to the events runner or
    external webhook.
    
    Also this won't likely work because there is no table for storing the
    event handlers at the moment, I need to figure out that one later.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- pkg/services/outgoingevents/service.go
+++ pkg/services/outgoingevents/service.go
@@ -1,14 +1,19 @@
 package outgoingevents
 
 import (
+	"context"
+	"fmt"
+	"sync"
+
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/services/sqlstore/db"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 type Service interface {
-	// Publish(event Event, payload interface{}) error
+	Publish(ctx context.Context, event Event, payload interface{}) error
 }
 
 type OutgoingEventsService struct {
@@ -29,4 +34,48 @@ func ProvideService(cfg *setting.Cfg, db db.DB, routeRegister routing.RouteRegis
 	}
 
 	return s
+}
+
+func (s *OutgoingEventsService) Publish(ctx context.Context, event Event, payload interface{}) error {
+	var handlers []EventHandler
+
+	err := s.db.WithDbSession(ctx, func(sess *sqlstore.DBSession) error {
+		return sess.Where("event = ?", event).Find(&handlers)
+	})
+	if err != nil {
+		return fmt.Errorf("listing event %s handlers: %w", event, err)
+	}
+
+	// If there are no handlers this method shouldn't fail, it just
+	// means no-one is interested in the event.
+
+	// TODO the number of workers should be configurable
+	const numWorkers = 3
+
+	var wg sync.WaitGroup
+	worker := func(handlers <-chan EventHandler) {
+		defer wg.Done()
+		wg.Add(1)
+
+		for h := range handlers {
+			if h.IsScript() {
+				// TODO send script to runner
+			} else {
+				// TODO send event to external webhook
+			}
+		}
+	}
+
+	jobs := make(chan EventHandler, len(handlers))
+	for w := 0; w < numWorkers; w++ {
+		go worker(jobs)
+	}
+	for _, handler := range handlers {
+		jobs <- handler
+	}
+	close(jobs)
+
+	wg.Wait()
+
+	return nil
 }
