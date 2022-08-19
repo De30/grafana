595ddc8dd44adddeb08f0138edc10b1bf1a786fd
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-08-19 10:30:46 -0300

    Borrow URL.JoinPath from standard library
    With this change we can revert the change to use Go 1.19.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- pkg/util/url.go
+++ pkg/util/url.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"net/url"
+	"path"
 	"strings"
 )
 
@@ -49,4 +50,65 @@ func JoinURLFragments(a, b string) string {
 		return a + "/" + b
 	}
 	return a + b
+}
+
+// URLJoinPath returns a URL string with the provided path elements
+// joined to the existing path of base and the resulting path cleaned
+// of any ./ or ../ elements.
+//
+// This is a copy of the url.JoinPath method introduced in Go 1.19. It
+// should be removed once Grafana sets the minimium Go version to that
+// one.
+func URLJoinPath(base string, elem ...string) (string, error) {
+	u, err := url.Parse(base)
+	if err != nil {
+		return "", err
+	}
+	return urlJoinPath(u, elem...).String(), nil
+}
+
+// urlJoinPath returns a new URL with the provided path elements
+// joined to any existing path and the resulting path cleaned of any
+// ./ or ../ elements. Any sequences of multiple / characters will be
+// reduced to a single /.
+//
+// This is a copy of the URL.JoinPath method introduced in Go 1.19. It
+// should be removed once Grafana sets the minimium Go version to that
+// one.
+func urlJoinPath(u *url.URL, elem ...string) *url.URL {
+	url := *u
+	if len(elem) > 0 {
+		elem = append([]string{u.EscapedPath()}, elem...)
+		p := path.Join(elem...)
+		// path.Join will remove any trailing slashes.
+		// Preserve at least one.
+		if strings.HasSuffix(elem[len(elem)-1], "/") && !strings.HasSuffix(p, "/") {
+			p += "/"
+		}
+		urlSetPath(&url, p)
+	}
+	return &url
+}
+
+// urlSetPath sets the Path and RawPath fields of the URL based on the provided
+// escaped path p. It maintains the invariant that RawPath is only specified
+// when it differs from the default encoding of the path.
+// For example:
+// - setPath("/foo/bar")   will set Path="/foo/bar" and RawPath=""
+// - setPath("/foo%2fbar") will set Path="/foo/bar" and RawPath="/foo%2fbar"
+// setPath will return an error only if the provided path contains an invalid
+// escaping.
+func urlSetPath(u *url.URL, p string) error {
+	path, err := url.PathUnescape(p)
+	if err != nil {
+		return err
+	}
+	u.Path = path
+	if escp := url.PathEscape(path); p == escp {
+		// Default encoding is fine.
+		u.RawPath = ""
+	} else {
+		u.RawPath = p
+	}
+	return nil
 }
