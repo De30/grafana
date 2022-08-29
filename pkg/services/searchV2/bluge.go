6b6b9dc82ed7a1ef1e7755f4984c9d2c45163927
Artur Wierzbicki <artur.wierzbicki@grafana.com>
2022-08-30 01:34:07 +0200

    prefix search

--- pkg/services/searchV2/bluge.go
+++ pkg/services/searchV2/bluge.go
@@ -231,7 +231,7 @@ func newSearchDocument(uid string, name string, descr string, url string) *bluge
 	if name != "" {
 		doc.AddField(bluge.NewTextField(documentFieldName, name).StoreValue().SearchTermPositions())
 		doc.AddField(bluge.NewTextField(documentFieldName_ngram, name).WithAnalyzer(ngramIndexAnalyzer))
-		doc.AddField(bluge.NewTextField(documentFieldName_nows, strings.ToLower(strings.ReplaceAll(name, " ", ""))).StoreValue().SearchTermPositions())
+		doc.AddField(bluge.NewTextField(documentFieldName_nows, strings.ReplaceAll(name, " ", "")).StoreValue().SearchTermPositions())
 
 		// Don't add a field for empty names
 		sortStr := strings.Trim(strings.ToUpper(name), " ")
@@ -439,7 +439,7 @@ func doSearchQuery(
 	} else {
 		// The actual se
 		bq := bluge.NewBooleanQuery().
-			AddShould(bluge.NewMatchQuery(q.Query).SetField(documentFieldName).SetBoost(1)).
+			AddShould(bluge.NewMatchQuery(q.Query).SetField(documentFieldName).SetBoost(6)).
 			AddShould(bluge.NewMatchQuery(q.Query).SetField(documentFieldDescription).SetBoost(3)).
 			AddShould(bluge.NewMatchQuery(q.Query).
 				SetField(documentFieldName_ngram).
@@ -450,8 +450,8 @@ func doSearchQuery(
 			bq.AddShould(bluge.NewFuzzyQuery(q.Query).SetField(documentFieldName)).SetBoost(1.5)
 		}
 
-		if len(q.Query) > 4 {
-			bq.AddShould(bluge.NewPrefixQuery(strings.ReplaceAll(strings.ToLower(q.Query), " ", "")).SetField(documentFieldName_nows)).SetBoost(6)
+		if len(q.Query) > ngramEdgeFilterMaxLength {
+			bq.AddShould(bluge.NewPrefixQuery(strings.ReplaceAll(q.Query, " ", "")).SetField(documentFieldName_nows)).SetBoost(6)
 		}
 		fullQuery.AddMust(bq)
 	}
