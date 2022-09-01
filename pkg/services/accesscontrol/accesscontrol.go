fed6f5b669f510f45e35916abefafe58212b23ec
Karl Persson <kalle.persson@grafana.com>
2022-09-01 12:37:29 +0200

    RBAC: Add comment

--- pkg/services/accesscontrol/accesscontrol.go
+++ pkg/services/accesscontrol/accesscontrol.go
@@ -15,6 +15,9 @@ import (
 type AccessControl interface {
 	// Evaluate evaluates access to the given resources.
 	Evaluate(ctx context.Context, user *user.SignedInUser, evaluator Evaluator) (bool, error)
+	// Checker builds an optimized checker function for a specific action and should be used
+	// when we need to check access to a resource type repeatedly
+	// Prefixes is used to generate possible wildcards for scopes related to the action
 	Checker(ctx context.Context, user *user.SignedInUser, action string, prefixes ...string) func(scopes ...string) bool
 	// RegisterScopeAttributeResolver allows the caller to register a scope resolver for a
 	// specific scope prefix (ex: datasources:name:)
