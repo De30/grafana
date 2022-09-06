135a50242bf7a1f6299cdc7771d6146bf322b2a8
joshhunt <josh@trtr.co>
2022-09-06 09:20:23 +0100

    add babel parser

--- scripts/cli/countUntranslatedStrings.ts
+++ scripts/cli/countUntranslatedStrings.ts
@@ -1,4 +1,6 @@
-import { readdir, stat } from 'fs/promises';
+import * as parser from '@babel/parser';
+import traverse from '@babel/traverse';
+import { readdir, readFile, stat } from 'fs/promises';
 import path from 'path';
 
 async function findFiles(dir: string, excludeFilter: RegExp): Promise<string[]> {
@@ -31,9 +33,27 @@ const FILE_EXCLUDE_FILTER = /node_modules|docs|\.git|\.yarn|e2e\/tmp/;
 async function main() {
   const root = path.resolve('./');
   const files = await findFiles(root, FILE_EXCLUDE_FILTER);
-  const codeFiles = files.filter((v) => v.match(/\.tsx?/) && !v.match(/\.(test|spec)\.tsx?/));
+  const codeFiles = files
+    .filter((v) => v.match(/\.tsx?/) && !v.match(/\.(test|spec)\.tsx?/))
+    .filter((v) => v.includes('Alert/Alert.tsx'));
 
-  console.log(codeFiles);
+  for (const codeFilePath of codeFiles) {
+    const source = await readFile(codeFilePath);
+
+    const ast = parser.parse(source.toString(), {
+      sourceFilename: codeFilePath,
+      sourceType: 'module',
+      plugins: ['typescript', 'jsx'],
+    });
+
+    traverse(ast, {
+      JSXAttribute: function (path) {
+        const { node } = path;
+
+        console.log(node);
+      },
+    });
+  }
 }
 
 main().catch(console.error);
