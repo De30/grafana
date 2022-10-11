90c350354cfc1d6a3809380502e3019d06dd70de
kay delaney <kay@grafana.com>
2022-10-11 11:06:45 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:public/test/helpers/getDashboardModel.ts

--- public/test/helpers/getDashboardModel.ts
+++ public/test/helpers/getDashboardModel.ts
@@ -1,4 +1,4 @@
-import { Dashboard, Panel } from '@grafana/schema';
+import { Dashboard, Panel, Transformation } from '@grafana/schema';
 
 import { DashboardModel, PanelModel } from '../../app/features/dashboard/state';
 
@@ -20,17 +20,44 @@ export const getDashboardModelMock = () => {
 
 // const dashboard = new DashboardMockBuilder().addPanel(panelBuilder.withTransformations([]).withTargets([])).
 
-
-const isPanelModel = (panel: Partial<Panel>): panel is Panel => {
-  return ((typeof panel === 'object') && (typeof panel.id === 'number'|| typeof panel.id === undefined)
-  && (typeof panel.type === 'string')); 
+function isPanel(panel: Partial<Panel>): panel is Panel {
+  return (
+    (typeof panel.id === 'number' || typeof panel.id === undefined) &&
+    typeof panel.type === 'string' &&
+    typeof panel.transparent === 'boolean' &&
+    Array.isArray(panel.transformations) &&
+    Boolean(panel.repeatDirection && ['h', 'v'].includes(panel.repeatDirection)) &&
+    Boolean(panel.options && Object.keys(panel.options).every((v) => typeof v === 'string')) &&
+    Boolean(
+      panel.fieldConfig && typeof panel.fieldConfig.defaults === 'object' && Array.isArray(panel.fieldConfig.overrides)
+    )
+  );
 }
 
 class PanelMockBuilder {
-  private mock: Partial<Panel> = {};
+  private mock: Partial<Panel> = { transparent: false };
   constructor() {}
 
   build(): Panel {
-    return this.mock;
+    if (isPanel(this.mock)) {
+      return this.mock;
+    } else {
+      throw new Error('this.mock is not a Panel!');
+    }
+  }
+
+  setType(type: string) {
+    this.mock.type = type;
+    return this;
+  }
+
+  setTransparent() {
+    this.mock.transparent = true;
+    return this;
+  }
+
+  addTransformation(...transformations: Transformation[]) {
+    this.mock.transformations = (this.mock.transformations ?? []).concat(...transformations);
+    return this;
   }
 }
