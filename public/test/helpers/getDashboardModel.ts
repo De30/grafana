0d379c0f4fd707a11f372dee0ba63fb65cfc2808
Alexandra Vargas <alexa1866@gmail.com>
2022-10-11 11:22:59 +0200

    dashboards-squad mob! :trident:

--- public/test/helpers/getDashboardModel.ts
+++ public/test/helpers/getDashboardModel.ts
@@ -1,6 +1,19 @@
+import { Dashboard } from '@grafana/schema';
+
 import { DashboardModel } from '../../app/features/dashboard/state';
 
 export const getDashboardModel = (json: any, meta: any = {}) => {
   const getVariablesFromState = () => json.templating.list;
   return new DashboardModel(json, meta, getVariablesFromState);
+};
+
+export const getDashboardModelMock = () => {
+  const dashboard: Dashboard = {
+    editable: true,
+    graphTooltip: 0,
+    revision: 1,
+    schemaVersion: 0,
+    style: 'dark',
+  };
+  return new DashboardModel(dashboard);
 };
