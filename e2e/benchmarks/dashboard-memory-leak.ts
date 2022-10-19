99a59c4805de3fe9f95d15b09b2c983755f3a25e
Alexandra Vargas <alexa1866@gmail.com>
2022-10-19 18:30:21 +0200

    Reduce the scope to basic dashboard with one panel

--- e2e/benchmarks/dashboard-memory-leak.ts
+++ e2e/benchmarks/dashboard-memory-leak.ts
@@ -1,10 +1,17 @@
+//Initial state
+const dashboardOnePanel = 'http://localhost:3000/d/WFkD6ySVz/dashboard-performacne-test-stat-panel?orgId=1';
+
 function url() {
-  return 'http://localhost:3000/d/I0YIojp7z/repeating-a-row-with-a-repeating-vertical-panel?orgId=1';
+  return dashboardOnePanel;
 }
+
+// Action to perform
 async function action(page) {
   await page.click('button[aria-label="Zoom out time range"]');
   await page.click('a[aria-label="Dashboards"]');
 }
+
+// Go back to initial state
 async function back(page) {
   await page.goBack();
 }
