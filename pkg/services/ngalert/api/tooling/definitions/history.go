a08733cafecbf07829d1cd649cb38aaae0925502
Alex Weaver <weaver.alex.d@gmail.com>
2022-11-28 16:46:57 -0600

    Define and generate endpoints

---
+++ pkg/services/ngalert/api/tooling/definitions/history.go
@@ -0,0 +1,27 @@
+package definitions
+
+import "github.com/grafana/grafana-plugin-sdk-go/data"
+
+// swagger:route GET /api/ruler/grafana/states history RouteGetStateHistory
+//
+// Query state history.
+//
+//     Produces:
+//     - application/json
+//
+//     Responses:
+//       200: StateHistory
+
+// swagger:route GET /api/ruler/grafana/states/{RuleUID} history RouteGetStateHistory
+//
+// Query state history for a rule.
+//
+//     Produces:
+//     - application/json
+//
+//     Responses:
+//       200: StateHistory
+
+type StateHistory struct {
+	results data.Frame
+}
