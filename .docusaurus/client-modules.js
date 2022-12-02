33c6f95b1dad41f8c95e41f79a8a2925ed5fbf2a
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-12-02 15:01:58 +0000

    Remove files that should be ignored

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/joeytawadrous/dev/grafana/.yarn/cache/infima-npm-0.2.0-alpha.42-6e892ed9a6-7206f36639.zip/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/joeytawadrous/dev/grafana/.yarn/__virtual__/@docusaurus-theme-classic-virtual-cfbcce30a0/0/cache/@docusaurus-theme-classic-npm-2.2.0-e81038b103-ccfb0bef12.zip/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/joeytawadrous/dev/grafana/.yarn/__virtual__/@docusaurus-theme-classic-virtual-cfbcce30a0/0/cache/@docusaurus-theme-classic-npm-2.2.0-e81038b103-ccfb0bef12.zip/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/joeytawadrous/dev/grafana/design-system/src/css/custom.css'),
-];
