e65e4cefb0ba5c5bc550cc535957d273539283b7
sam boyer <sdboyer@grafana.com>
2022-12-05 16:12:16 -0500

    Add platformaton backend service w/sketchy outline

---
+++ pkg/services/platformaton/sillyloop.go
@@ -0,0 +1,53 @@
+package platformaton
+
+import (
+	"context"
+
+	"github.com/grafana/grafana/pkg/infra/log"
+	"github.com/grafana/grafana/pkg/registry/corekind"
+	"github.com/grafana/grafana/pkg/services/store/entity"
+)
+
+// We can do one of these platformatons per file - this'll be the first, for SM.
+// k8s app can go in a separate file
+
+func ProvideSMPlatformaton(store entity.EntityStoreServer, kreg corekind.Base) *SMPlato {
+	return &SMPlato{
+		log:   log.New("platformaton-sm"),
+		store: store,
+		kreg:  kreg,
+	}
+}
+
+type SMPlato struct {
+	log   log.Logger
+	store entity.EntityStoreServer
+	kreg  corekind.Base
+}
+
+func (p *SMPlato) Run(ctx context.Context) error {
+	// p.store.Watch(// pass a kind name here) // get a channel back
+}
+
+// We care about
+// 1. platformaton config
+// 2. state of all types of matched inputs
+
+func (p *SMPlato) Reconcile() error {
+	// 1. load all entities we could possibly care about
+	// 2. load all known transformaton configs from storage
+	// 3. run generation process for all input uids according to loaded tf configs
+	// 4. save generated objects to appropriate targets (local storage + SM API)
+
+	ksvc := p.kreg.Service()
+
+	// Pretend these bytes came back from an p.store.Read() call
+	var b []byte
+	// Transform the bytes into
+	svc, _, err := ksvc.JSONValueMux(b)
+	// repeat to load platformaton config
+
+	// then, implement logic to create checks, etc. from svc + pfsm
+	// then, write 'em to the places they need to go
+
+}
