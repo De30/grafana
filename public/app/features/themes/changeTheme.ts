71946d24df974db18c06e7b8865ef665fc83602e
Torkel Ödegaard <torkel@grafana.com>
2022-12-08 13:56:31 +0100

    Updates

--- public/app/features/themes/changeTheme.ts
+++ public/app/features/themes/changeTheme.ts
@@ -1,6 +1,8 @@
-import { createTheme } from '@grafana/data';
+import { createTheme, GrafanaTheme2 } from '@grafana/data';
 import { config, ThemeChangedEvent } from '@grafana/runtime';
 import appEvents from 'app/core/app_events';
+
+import { getDashboardSrv } from '../dashboard/services/DashboardSrv';
 
 import { CustomThemeDTO } from './state';
 
@@ -13,13 +15,34 @@ export function setRuntimeTheme(custom: CustomThemeDTO, safeMode?: boolean) {
   };
 
   try {
+    const currentTheme = config.theme2;
     const runtimeTheme = createTheme(options);
     if (!safeMode) {
       appEvents.publish(new ThemeChangedEvent(runtimeTheme));
     }
+
+    if (areVizColorsDifferent(currentTheme, runtimeTheme)) {
+      console.log('colors are dif');
+      setTimeout(() => {
+        getDashboardSrv().getCurrent()?.refresh();
+      }, 100);
+    }
+
     return runtimeTheme;
   } catch (err: unknown) {
     console.error(err);
     return config.theme2;
   }
+}
+
+function areVizColorsDifferent(theme: GrafanaTheme2, newTheme: GrafanaTheme2) {
+  for (const [hueIndex, hue] of theme.visualization.hues.entries()) {
+    for (const [colorIndex, color] of hue.shades.entries()) {
+      if (color !== newTheme.visualization.hues[hueIndex].shades[colorIndex]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
