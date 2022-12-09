a1067511ab641e74653c91a205fbe09a7f3f7d37
Dominik Prokop <dominik.prokop@grafana.com>
2022-12-09 12:25:28 +0100

    Animation alternative

--- public/app/features/dashboard/components/LoadingBar/LoadingBar.tsx
+++ public/app/features/dashboard/components/LoadingBar/LoadingBar.tsx
@@ -1,7 +1,6 @@
 import { css, keyframes } from '@emotion/css';
 import React from 'react';
 
-import { GrafanaTheme2 } from '@grafana/data';
 import { useStyles2 } from '@grafana/ui/src/themes';
 
 /**
@@ -17,38 +16,47 @@ export interface LoadingBarProps {
  * @internal
  */
 export function LoadingBar({ width, height, ariaLabel = 'Loading bar' }: LoadingBarProps) {
-  const styles = useStyles2(getStyles(width, height));
-
+  const styles = useStyles2(getStyles);
+  const barWidth = width ?? '128px';
+  const loadingHeigth = height ?? '2px';
+  const barStyles = {
+    background: 'linear-gradient(90deg, rgba(110, 159, 255, 0) 0%, #6E9FFF 80.75%, rgba(110, 159, 255, 0) 100%)',
+    width: `${barWidth}`,
+    height: `${loadingHeigth}`,
+  };
   return (
     <div className={styles.container}>
-      <div aria-label={ariaLabel} className={styles.bar} />
+      <div aria-label={ariaLabel} className={styles.bar1} style={barStyles} />
+      <div aria-label={ariaLabel} className={styles.bar2} style={barStyles} />
     </div>
   );
 }
 
-const getStyles = (width?: string, height?: string) => (_: GrafanaTheme2) => {
-  const barWidth = width ?? '128px';
-  const loadingHeigth = height ?? '2px';
-
+const getStyles = () => {
   const loadingAnimation = keyframes({
     '0%': {
       transform: 'translateX(0)',
     },
     '100%': {
-      transform: `translateX(calc(100% - ${barWidth}))`,
+      transform: `translateX(calc(100%))`,
     },
   });
 
   return {
     container: css({
       width: '100%',
-      animation: `${loadingAnimation} 2s infinite linear`,
+      animation: `${loadingAnimation} 1s infinite linear`,
       willChange: 'transform',
     }),
-    bar: css({
-      width: barWidth,
-      height: loadingHeigth,
-      background: 'linear-gradient(90deg, rgba(110, 159, 255, 0) 0%, #6E9FFF 80.75%, rgba(110, 159, 255, 0) 100%)',
+    bar1: css({
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }),
+    bar2: css({
+      position: 'absolute',
+      top: 0,
+      left: '-100%',
     }),
   };
 };
