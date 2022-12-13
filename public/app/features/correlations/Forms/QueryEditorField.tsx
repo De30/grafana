23df883e688b914751c0aa934c1aaf52dcc44431
Giordano Ricci <me@giordanoricci.com>
2022-12-13 07:19:44 +0000

    Revert "refactor: replace query runner"
    This reverts commit aac76e073acb2077a7851d29b7411a3e096f30bb.

--- public/app/features/correlations/Forms/QueryEditorField.tsx
+++ public/app/features/correlations/Forms/QueryEditorField.tsx
@@ -1,10 +1,10 @@
 import { css } from '@emotion/css';
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Controller } from 'react-hook-form';
 import { useAsync } from 'react-use';
 
 import { DataQuery, getDefaultTimeRange, GrafanaTheme2 } from '@grafana/data';
-import { getDataSourceSrv } from '@grafana/runtime';
+import { createQueryRunner, getDataSourceSrv } from '@grafana/runtime';
 import {
   Field,
   LoadingPlaceholder,
@@ -15,10 +15,6 @@ import {
   FieldValidationMessage,
   useStyles2,
 } from '@grafana/ui';
-
-import { buildQueryTransaction } from '../../../core/utils/explore';
-import { ExploreId } from '../../../types';
-import { runRequest } from '../../query/state/runRequest';
 
 interface Props {
   dsUid?: string;
@@ -40,6 +36,21 @@ export const QueryEditorField = ({ dsUid, invalid, error, name }: Props) => {
 
   const style = useStyles2(getStyle);
 
+  const runner = useMemo(createQueryRunner, []);
+
+  useEffect(() => {
+    runner.get().subscribe((panelData) => {
+      if (!panelData || panelData.state === 'Error') {
+        setIsValidQuery(false);
+      } else if (panelData.state === 'Done') {
+        setIsValidQuery(true);
+      } else {
+        setIsValidQuery(undefined);
+      }
+      return runner.destroy;
+    });
+  }, [runner]);
+
   const {
     value: datasource,
     loading: dsLoading,
@@ -53,17 +64,14 @@ export const QueryEditorField = ({ dsUid, invalid, error, name }: Props) => {
   const QueryEditor = datasource?.components?.QueryEditor;
 
   const handleValidation = (value: DataQuery) => {
-    const transaction = buildQueryTransaction(ExploreId.left, [value], {}, getDefaultTimeRange(), false, 'utc');
-
     if (datasource) {
-      runRequest(datasource, transaction.request).subscribe((panelData) => {
-        if (!panelData || panelData.state === 'Error') {
-          setIsValidQuery(false);
-        } else if (panelData.state === 'Done') {
-          setIsValidQuery(true);
-        } else {
-          setIsValidQuery(undefined);
-        }
+      runner.run({
+        datasource: datasource,
+        queries: [value],
+        timezone: 'utc',
+        timeRange: getDefaultTimeRange(),
+        maxDataPoints: 100,
+        minInterval: null,
       });
     }
   };
